da6dc77f08e8deabc370a8e1bee97739070f0ea4
unknown <ka_soualehmohamed@esi.dz>
2023-02-09 21:44:24 +0100

    a constructor for struct

--- core/include/traccc/edm/cell.hpp
+++ core/include/traccc/edm/cell.hpp
@@ -118,7 +118,7 @@ struct CellVecDevice {
     int_device cluster_id;
     
     /// constructor 
-    CellVecDevice(const traccc::cuda::CellView& data);
+    CellVecDevice(const CellView& data);
     
 };
 
