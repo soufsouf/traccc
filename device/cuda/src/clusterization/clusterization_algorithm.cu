95788d7fa969d815d5bb9ce417dd09d5fd2fbd5c
maissa <im_boukaf@esi.dz>
2023-02-14 01:00:40 -1100

    try to solve the problem

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -342,7 +342,7 @@ printf("capacity : %llu " ,cells_prefix_sum_buff.capacity());*/
          
     m_copy(cl_per_module_prefix_buff, cl_per_module_prefix_host,
            vecmem::copy::type::copy_type::device_to_host);
-          for(int j = 5; j<20;j++) printf("host avant IS : %llu *** \n",cl_per_module_prefix_host[j] );
+         // for(int j = 5; j<20;j++) printf("host avant IS : %llu *** \n",cl_per_module_prefix_host[j] );
     //m_stream.synchronize();
     std::vector<std::size_t> clusters_per_module_host(
         cl_per_module_prefix_host.begin(), cl_per_module_prefix_host.end());
