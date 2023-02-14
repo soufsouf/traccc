fe58aa055cbad88ace16b6910f7d5c02504a46ef
maissa <im_boukaf@esi.dz>
2023-02-13 23:27:34 -1100

    try to solve the problem

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -343,7 +343,7 @@ printf("capacity : %llu " ,cells_prefix_sum_buff.capacity());*/
         m_mr.host ? m_mr.host : &(m_mr.main));
     m_copy(cl_per_module_prefix_buff, cl_per_module_prefix_host,
            vecmem::copy::type::copy_type::device_to_host);
-           for(int j = 230; j<261;j++) printf("host avant IS : %llu *** \n",cl_per_module_prefix_host[j] );
+        //   for(int j = 230; j<261;j++) printf("host avant IS : %llu *** \n",cl_per_module_prefix_host[j] );
     //m_stream.synchronize();
     std::vector<std::size_t> clusters_per_module_host(
         cl_per_module_prefix_host.begin(), cl_per_module_prefix_host.end());
@@ -354,7 +354,7 @@ printf("capacity : %llu " ,cells_prefix_sum_buff.capacity());*/
                         cl_per_module_prefix_host.begin());
 
     unsigned int total_clusters = cl_per_module_prefix_host.back();
-for(int i = 230; i<261;i++) printf("host : %llu \n",cl_per_module_prefix_host[i] );
+//for(int i = 230; i<261;i++) printf("host : %llu \n",cl_per_module_prefix_host[i] );
     // Copy the prefix sum back to its device container
     m_copy(vecmem::get_data(cl_per_module_prefix_host),
            cl_per_module_prefix_buff,
