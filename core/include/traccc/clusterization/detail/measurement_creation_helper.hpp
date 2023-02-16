ff4357ecb7b62bfa3fdffab052a6dd0daa2a2b3f
unknown <ka_soualehmohamed@esi.dz>
2023-02-17 00:32:13 +0100

    error

--- core/include/traccc/clusterization/detail/measurement_creation_helper.hpp
+++ core/include/traccc/clusterization/detail/measurement_creation_helper.hpp
@@ -125,7 +125,7 @@ void calc_cluster_properties(
             }
         }
     }
-    if (cl_link< 8) 
+    if (cl_link > 1111 &&  cl_link < 1114) 
     printf("mean [0] %llu  mean [1] %llu var[0] %llu var[1] %llu\n",
             mean[0] , mean[1] , var[0] , var[1]); 
 }
