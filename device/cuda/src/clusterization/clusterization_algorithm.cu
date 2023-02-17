f415da5ae26f020b36dcd294938f2c7bc68e162a
maissa <im_boukaf@esi.dz>
2023-02-16 15:37:17 -1100

    create header struct

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -475,7 +475,7 @@ printf("capacity : %llu " ,cells_prefix_sum_buff.capacity());*/
     
     spacepoint_container_types::host spacepoint_host;
     spacepoint_host = spacepoint_copy(spacepoints_buffer);
-    auto sp = spacepoint_host.get_headers().at(24);
+    auto sp = spacepoint_host.get_headers().at(72);
     printf("hello mismis %llu  | sp = %llu \n", spacepoint_host.total_size(), sp);
     
 
