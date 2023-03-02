f648543dbbee2ac93dd7053fed210e6976a80416
maissa <im_boukaf@esi.dz>
2023-03-02 03:06:48 -1100

    print

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -205,8 +205,8 @@ __global__ void ccl_kernel(
     //print 1
     //printf(" bloc: %u | thread: %u | target cells per partition %u | start : %u | end : %u \n", blockIdx.x, tid , target_cells_per_partition,start,end);
     // print 5
-    for (int i = 71699 ; i < 71707 ; i ++) 
-          printf(" cells_device [%u] = %u \n", i , cells_device [i] );
+    for (int i = 71698 ; i < 71707 ; i ++) 
+          printf(" cells_device [%u] = %u \n", i , cells_device [i].c.channel1 );
     const index_t size = end - start;
     assert(size <= max_cells_per_partition);
 
