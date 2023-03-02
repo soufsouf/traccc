274b5336c5c94817b27d88c7b33d6f85017001cd
maissa <im_boukaf@esi.dz>
2023-03-02 03:37:34 -1100

    print

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -203,8 +203,7 @@ __global__ void ccl_kernel(
     }
     __syncthreads();
     //print 1
-    if (blockIdx.x ==70)
-    printf(" bloc: %u | thread: %u | target cells per partition %u | start : %u | end : %u \n", blockIdx.x, tid , target_cells_per_partition,start,end);
+    if (blockIdx.x ==70) printf(" bloc: %u | thread: %u | target cells per partition %u | start : %u | end : %u \n", blockIdx.x, tid , target_cells_per_partition,start,end);
     // print 5
     /*for (int i = 71698 ; i < 71707 ; i ++) 
           printf(" cells_device [%u] = %u \n", i , cells_device [i].c.channel1 );*/
