9210c5e315d3d8cb4cb60d1b209888a7da34f8f7
maissa <im_boukaf@esi.dz>
2023-03-04 13:55:20 -1100

    print adjv

--- core/include/traccc/edm/cell.hpp
+++ core/include/traccc/edm/cell.hpp
@@ -29,7 +29,7 @@ struct cell {
     channel_id channel1 = 0;
     scalar activation = 0.;
     scalar time = 0.;
-    unsigned int cluster_indice = 2000;
+    unsigned int cluster_indice ;
 };
 
 /// Comparison / ordering operator for cells
