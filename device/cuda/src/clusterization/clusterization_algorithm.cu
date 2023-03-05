68124fc145667ce186d5902669951bd41619b46c
unknown <ka_soualehmohamed@esi.dz>
2023-03-05 19:42:48 +0100

    size calculation by all threads

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -227,10 +227,10 @@ __global__ void ccl_kernel(
         
         const index_t cell_id = iter * blckDim + tid;   /// cell_id : id de cell dans la partition 
         if (start == 0 ) break;
-        if ( cells_device[start + cell_id - 1].module_link !=
-                cells_device[start + cell_id].module_link /*&&
+        if ( cells_device[start + cell_id - 1].module_link ==
+                cells_device[start + cell_id].module_link &&
                 cells_device[start + cell_id].c.channel1 <=
-                cells_device[start + cell_id - 1].c.channel1 + 1*/) {
+                cells_device[start + cell_id - 1].c.channel1 + 1) {
                      printf(" i'm in 1 \n ");
                       cell = cell_id;
                     }
@@ -251,10 +251,10 @@ __global__ void ccl_kernel(
         
         const index_t cell_id = iter * blckDim + tid;
         
-        if ( end < num_cells && cells_device[end + cell_id - 1].module_link !=
-                   cells_device[end + cell_id].module_link /*&&
+        if ( end < num_cells && cells_device[end + cell_id - 1].module_link ==
+                   cells_device[end + cell_id].module_link &&
                cells_device[end + cell_id].c.channel1 <=
-                   cells_device[end + cell_id - 1].c.channel1 + 1*/) {
+                   cells_device[end + cell_id - 1].c.channel1 + 1) {
                     printf(" i'm in 2 \n ");
                     cell = cell_id;
                     }  /// if : end >= num_cells , the value of "end" will not change 
