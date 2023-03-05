08239a739311bd972c49ee8ffd5b3f8ee62a154e
unknown <ka_soualehmohamed@esi.dz>
2023-03-05 22:23:24 +0100

    size calculation by all threads

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -293,7 +293,7 @@ __global__ void ccl_kernel(
     
 
     const index_t size = end - start;
-    //printf(" size %hu", size);
+    printf(" size %hu", size);
     assert(size <= max_cells_per_partition);
 
     // Check if any work needs to be done
