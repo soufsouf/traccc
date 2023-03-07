1643b017c5e03ff3531a48cc65da04608eae0eb6
maissa <im_boukaf@esi.dz>
2023-03-07 07:35:53 -1100

    print adjv

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -237,7 +237,8 @@ printf(" hello before declaration of shared variables \n");
     extern __shared__ idx_cluster cluster_id[];
     grp_cluster* cluster_group = &cluster_vector[0];
     idx_cluster* index = &cluster_id[0];
-    __shared__ unsigned int cluster_count = 0;
+    __shared__ unsigned int cluster_count ;
+    cluster_count =0;
 printf(" after declaration of shared variables \n");
 #pragma unroll
    
