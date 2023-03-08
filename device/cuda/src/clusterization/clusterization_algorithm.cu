b195aa70b5115a957e3382424a202d5b07783eda
maissa <im_boukaf@esi.dz>
2023-03-08 12:09:13 -1100

    solution 4

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -256,7 +256,7 @@ printf(" after declaration of shared variables \n");
     comp_id compare;
     thrust::device_ptr<index_t> devPtr(cluster_group);
     //= thrust::device_pointer_cast(shared_data)
-    thrust::radix_sort(devPtr, devPtr + max_cells_per_partition,compare );
+    thrust::stable_sort(thrust::cuda::par, devPtr, devPtr + max_cells_per_partition,compare );
 
      if (tid == 0) {
         outi = atomicAdd(&measurement_count, cluster_count);
