7aa1dfe45b5095914841a2741d9d928f928ce8a6
unknown <ka_soualehmohamed@esi.dz>
2023-03-09 13:12:33 +0100

    changing m_target_cells_per_partition

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -454,11 +454,15 @@ clusterization_algorithm::output_type clusterization_algorithm::operator()(
     CUDA_ERROR_CHECK(
         cudaMemset(num_measurements_device.get(), 0, sizeof(unsigned int)));
 
-    int target  = 6000;
+    // Create CUDA events
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
     const unsigned short max_cells_per_partition =
         (m_target_cells_per_partition * MAX_CELLS_PER_THREAD +
          TARGET_CELLS_PER_THREAD - 1) /
-        TARGET_CELLS_PER_THREAD; 
+        TARGET_CELLS_PER_THREAD;
     const unsigned int threads_per_partition =
         (m_target_cells_per_partition + TARGET_CELLS_PER_THREAD - 1) /
         TARGET_CELLS_PER_THREAD;
@@ -467,14 +471,24 @@ clusterization_algorithm::output_type clusterization_algorithm::operator()(
         m_target_cells_per_partition;
 
     // Launch ccl kernel. Each thread will handle a single cell.
+      cudaEventRecord(start);
     kernels::
         ccl_kernel<<<num_partitions, threads_per_partition,
-                     target * sizeof(index_t), stream>>>(
+                     2 * max_cells_per_partition * sizeof(index_t), stream>>>(
             cells, modules, max_cells_per_partition,
             m_target_cells_per_partition, measurements_buffer,
             *num_measurements_device);
 
     CUDA_ERROR_CHECK(cudaGetLastError());
+
+      cudaEventRecord(stop);
+      cudaEventSynchronize(stop);
+
+    // Calculate the elapsed time
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    printf("Elapsed time: %f ms\n", milliseconds);
     // Copy number of measurements to host
     vecmem::unique_alloc_ptr<unsigned int> num_measurements_host =
         vecmem::make_unique_alloc<unsigned int>(*(m_mr.host));
