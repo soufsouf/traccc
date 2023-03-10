222ca336fed14fdd77635139dfa0d608e4eb73b5
unknown <ka_soualehmohamed@esi.dz>
2023-03-10 14:08:52 +0100

    64 thread per block attempt

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -243,7 +243,8 @@ __global__ void ccl_kernel(
         // thread with lane id 0 writes the result 
         int warpId = tid / warpSize;  
         int warp_min1;
-        int warp_min2;      
+        int warp_min2; 
+        cell = 999     
         if ( warpId == 0 ) warp_min1 = warpReduceMin(cell);
         if ( warpId == 1 ) warp_min2 = warpReduceMin(cell);
         __syncthreads(); /// we need it in 64 thread per block 
