bf110d5400a6999791c16346c15b3dbcae3d989c
unknown <ka_soualehmohamed@esi.dz>
2023-03-10 13:21:22 +0100

    64 thread per block attempt

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -481,7 +481,7 @@ clusterization_algorithm::output_type clusterization_algorithm::operator()(
       
     kernels::
         ccl_kernel<<<num_partitions, threads_per_partition,
-                     3072 * sizeof(index_t), stream>>>(
+                     max_cells_per_partition * sizeof(index_t), stream>>>(
             cells, modules, max_cells_per_partition,
             m_target_cells_per_partition, measurements_buffer,
             *num_measurements_device);
