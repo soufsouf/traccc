cf367b4fd560cc44c0fb6fa4e8c0543fb7087202
maissa <im_boukaf@esi.dz>
2023-03-12 09:54:30 -1100

    hello

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -162,8 +162,8 @@ __global__ void ccl_kernel(
     __shared__ unsigned int outi;
     extern __shared__ index_t fathers[];
     index_t* id_fathers = &fathers[0];
-    index_t* f = &fathers[max_cells_per_partition];
-    index_t* f_next = &fathers[2*max_cells_per_partition];
+    /*index_t* f = &fathers[max_cells_per_partition];
+    index_t* f_next = &fathers[2*max_cells_per_partition];*/
     /*
      * First, we determine the exact range of cells that is to be examined by
      * this block of threads. We start from an initial range determined by the
@@ -402,7 +402,7 @@ clusterization_algorithm::output_type clusterization_algorithm::operator()(
     // Launch ccl kernel. Each thread will handle a single cell.
     kernels::
         ccl_kernel<<<num_partitions, threads_per_partition,
-                     3 * max_cells_per_partition * sizeof(index_t), stream>>>(
+                      max_cells_per_partition * sizeof(index_t), stream>>>(
             cells, modules, max_cells_per_partition,
             m_target_cells_per_partition, measurements_buffer,
             *num_measurements_device, cell_links);
