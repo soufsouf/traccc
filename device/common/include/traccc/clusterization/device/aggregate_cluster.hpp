ea1949623a1db2bdd7d6c74aaf8c4332b3173988
maissa <im_boukaf@esi.dz>
2023-03-12 23:25:17 -1100

    integrate form spacepoints

--- device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
+++ device/common/include/traccc/clusterization/device/aggregate_cluster.hpp
@@ -39,8 +39,7 @@ inline void aggregate_cluster(
     const unsigned int start, const unsigned int end, const unsigned short cid,
     spacepoint_collection_types::view spacepoints_view,
      vecmem::data::vector_view<unsigned int> cell_links,
-    const unsigned int link,
-    const unsigned int id);
+    const unsigned int link);
 
 }  // namespace traccc::device
 
