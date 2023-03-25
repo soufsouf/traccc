d891d7107deba81b88efe02f9ac08f6ecf4d3642
unknown <ka_soualehmohamed@esi.dz>
2023-03-25 23:26:56 +0100

    avoid bank conflict in f

--- device/cuda/src/clusterization/clusterization_algorithm.cu
+++ device/cuda/src/clusterization/clusterization_algorithm.cu
@@ -313,7 +313,7 @@ __global__ void ccl_kernel(
          */
         //printf (" adjv[tst][8] %u  block id %u cid %u  \n" , adjv[tst][8] , blockIdx.x, cid); 
         f[ccid] = adjv[tst][8];
-        printf(" f %hu tid %hu \n", f[ccid] , adjv[tst][8]);
+        //printf(" f %hu tid %hu \n", f[ccid] , adjv[tst][8]);
         
         //printf (" adjv[tst][8] %u \n" , adjv[tst][8]); 
     }
@@ -342,7 +342,7 @@ __global__ void ccl_kernel(
      */
     for (index_t tst = 0, cid; (cid = tst * blckDim + tid) < size; ++tst) {
         const index_t ccid = tst + tid*MAX_CELLS_PER_THREAD;
-        //printf(" f %u tid %u \n", f[ccid] , tid);
+        printf(" f %u tid %u \n", f[ccid] , tid);
         if (f[ccid] == cid) {
             atomicAdd(&outi, 1);
 
